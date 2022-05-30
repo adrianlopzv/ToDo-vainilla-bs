e165b5b3a62447eac22b712e27c6205dac7d5644
Adrian Lopez <adrian.08lv@gmail.com>
2022-05-30 14:42:13 -0300

    ToDo funcionando, tareas listas se subrayan, se puede borrar y restaurar para volver a completar

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const template = document.getElementById('template').content //siempre va conten
 const fragment = document.createDocumentFragment() //Crea un nuevo DocumentFragment vacio, dentro del cual un nodo del DOM puede ser adicionado para construir un nuevo arbol DOM fuera de pantalla
 
 let tasksList = {
-    160: {
-        id: 160,
-        texto: 'Tarea #1',
-        estado: false
-    }
+    // 160: {
+    //     id: 160,
+    //     texto: 'Tarea #1',
+    //     estado: false
+    // }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -56,11 +56,25 @@ const setTask = e => {
 }
 
 const printTask = () => {
+    if(Object.values(tasksList).length === 0) {
+        listaTarea.innerHTML = `
+        <div class="alert alert-dark "> All tasks done! ✨</div>
+        `
+        return
+    }
+
     listaTarea.innerHTML = null //pa limpiar el DOM y que empiece vacio
     Object.values(tasksList).forEach(task => {
-        const clone = template.cloneNode(true)
+        const clone = template.cloneNode(true) // importante que esto este al principio para ir modificando sobre el clone
         //se va a clonar todo lo que esta en la etiqueta template
         clone.querySelector('p').textContent = task.texto
+
+        if(task.estado) {
+            clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary')
+            clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle', 'fa-undo-alt')
+            clone.querySelector('p').style.textDecoration = 'line-through'
+        }
+
         clone.querySelectorAll('.fas')[0].dataset.id = task.id
         clone.querySelectorAll('.fas')[1].dataset.id = task.id
         fragment.appendChild(clone)
@@ -70,10 +84,21 @@ const printTask = () => {
 
 const btnAccion = e => {
     if(e.target.classList.contains('fa-check-circle')) {
-        console.log(e.target.dataset.id)
+        // console.log(e.target.dataset.id)
         tasksList[e.target.dataset.id].estado = true
         printTask()
         console.log(tasksList)
     }
+
+    if(e.target.classList.contains('fa-minus-circle')) {
+        delete tasksList[e.target.dataset.id]
+        printTask()
+    }
+
+    if(e.target.classList.contains('fa-undo-alt')) {
+        tasksList[e.target.dataset.id].estado = false
+        printTask()
+    }
+
     e.stopPropagation() //es para que solo se active ESTE evento y no los demas
 }
